c16521dd0543a76c6db799056f6d6e7143106103
shahir <shahirsammun00@gmail.com>
2023-06-23 21:56:10 +0600

    Code Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,182 +1,146 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PhotoGalleryApp());
+  runApp(MyApp());
 }
 
-class PhotoGalleryApp extends StatelessWidget {
-  const PhotoGalleryApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo Gallery',
+      title: 'Product List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PhotoGalleryScreen(),
+      home: const ProductList(),
     );
   }
 }
 
-class PhotoGalleryScreen extends StatelessWidget {
-  const PhotoGalleryScreen({super.key});
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  List<Product> products = [
+    Product(name: 'Cake', price: 300),
+    Product(name: 'Chips', price: 20),
+    Product(name: 'Biscuit', price: 70),
+    Product(name: 'Drinks', price: 80),
+    Product(name: 'Milk', price: 350),
+    Product(name: 'Pastry', price: 400),
+
+
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Photo Gallery'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.all(16.0),
-              child: Text(
-                'Welcome to My Photo Gallery!',
-                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 12.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search for photos',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.all(12.0),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(16.0),
-              child: Wrap(
-                spacing: 10.0,
-                runSpacing: 10.0,
-                children: [
-                  _buildPhotoButton(
-                    imageUrl: 'https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                    caption: 'Photo 1',
-                    onPressed: () {
-                      _showSnackBar(context, 'Clicked on Photo 1!');
-                    },
-                  ),
-                  _buildPhotoButton(
-                    imageUrl: 'https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                    caption: 'Photo 2',
-                    onPressed: () {
-                      _showSnackBar(context, 'Clicked on Photo 2!');
-                    },
-                  ),
-                  _buildPhotoButton(
-                    imageUrl: 'https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                    caption: 'Photo 3',
-                    onPressed: () {
-                      _showSnackBar(context, 'Clicked on Photo 3!');
-                    },
-                  ),
-                  _buildPhotoButton(
-                    imageUrl: 'https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                    caption: 'Photo 4',
-                    onPressed: () {
-                      _showSnackBar(context, 'Clicked on Photo 4!');
-                    },
-                  ),
-                  _buildPhotoButton(
-                    imageUrl: 'https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                    caption: 'Photo 5',
-                    onPressed: () {
-                      _showSnackBar(context, 'Clicked on Photo 5!');
-                    },
-                  ),
-                  _buildPhotoButton(
-                    imageUrl: 'https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-                    caption: 'Photo 6',
-                    onPressed: () {
-                      _showSnackBar(context, 'Clicked on Photo 6!');
-                    },
-                  ),
-                ],
-              ),
-            ),
-            ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: [
-                ListTile(
-                  leading: Icon(Icons.photo),
-                  title: Text('Sample Photo 1'),
-                  subtitle: Text('Category 1'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.photo),
-                  title: Text('Sample Photo 2'),
-                  subtitle: Text('Category 2'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.photo),
-                  title: Text('Sample Photo 3'),
-                  subtitle: Text('Category 3'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showSnackBar(context, 'Photos Uploaded Successfully!');
-        },
-        child: Icon(Icons.cloud_upload),
-      ),
-    );
-  }
-
-  Widget _buildPhotoButton({
-    required String imageUrl,
-    required String caption,
-    required VoidCallback onPressed,
-  }) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.zero,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
-            child: Image.network(
-              imageUrl,
-              width: 105.0,
-              height: 50.0,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              caption,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
+        title:Text('Product List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CartPage(products: products)),
+              );
+            },
           ),
         ],
       ),
-    );
-  }
-
-  void _showSnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ProductItem(
+            product: products[index],
+            onBuyNowPressed: () {
+              setState(() {
+                products[index].incrementCounter();
+                if (products[index].counter == 5) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Congratulations!'),
+                      content: Text('You\'ve bought 5 ${products[index].name}!'),
+                      actions: [
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              });
+            },
+          );
+        },
       ),
     );
   }
 }
 
+class Product {
+  final String name;
+  final double price;
+  int counter;
+
+  Product({required this.name, required this.price, this.counter = 0});
+
+  void incrementCounter() {
+    counter++;
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  final VoidCallback onBuyNowPressed;
+
+  ProductItem({required this.product, required this.onBuyNowPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(product.name),
+      subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
+      trailing: TextButton(
+        child: Text('Buy Now (${product.counter})'),
+        onPressed: onBuyNowPressed,
+      ),
+    );
+  }
+}
+
+class CartPage extends StatelessWidget {
+  final List<Product> products;
+
+  CartPage({required this.products});
+
+  int getTotalBought() {
+    int total = 0;
+    for (var product in products) {
+      total += product.counter;
+    }
+    return total;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Center(
+        child: Text('Total Products Bought: ${getTotalBought()}'),
+      ),
+    );
+  }
+}
 
